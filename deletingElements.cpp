990f5e82a85c50ff9e3a67c224874c9922ecbdce
Suhana0921 <73785663+Suhana0921@users.noreply.github.com>
2023-01-03 23:29:30 +0530

    Create deletingElements.cpp

---
+++ deletingElements.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<list> // for list operations
+using namespace std;
+  
+int main()
+{
+    list<int> list1={10,15,20,25,30,35};
+      
+    // printing original list
+    cout << "The original list is : ";
+    for (list<int>::iterator i=list1.begin(); i!=list1.end(); i++)
+       cout << *i << " ";
+       
+    cout << endl;
+
+    list1.pop_front();
+
+    cout << "The list after deleting first element using pop_front() : ";
+    for (list<int>::iterator i=list1.begin(); i!=list1.end(); i++)
+       cout << *i << " ";
+       
+    cout << endl;
+
+    list1.pop_back();
+
+    cout << "The list after deleting last element using pop_back() : ";
+    for (list<int>::iterator i=list1.begin(); i!=list1.end(); i++)
+       cout << *i << " ";
+       
+    cout << endl;
+      
+}
