a15a5ea9a87c50ba9cb8c50e7027309ecce3dcb5
Suhana0921 <73785663+Suhana0921@users.noreply.github.com>
2023-01-04 22:22:56 +0530

    Update MinimumNumberOfJumpsToReachEnd.cpp

--- MinimumNumberOfJumpsToReachEnd.cpp
+++ MinimumNumberOfJumpsToReachEnd.cpp
@@ -1,21 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-// Function to return the minimum number
-// of jumps to reach arr[h] from arr[l]
+
 int minJumps(int arr[], int n)
 {
- 
-    // Base case: when source and
-    // destination are same
+
     if (n == 1)
         return 0;
- 
-    // Traverse through all the points
-    // reachable from arr[l]
-    // Recursively, get the minimum number
-    // of jumps needed to reach arr[h] from
-    // these reachable points
+
     int res = INT_MAX;
     for (int i = n - 2; i >= 0; i--) {
         if (i + arr[i] >= n - 1) {
@@ -28,7 +19,6 @@ int minJumps(int arr[], int n)
     return res;
 }
  
-// Driver Code
 int main()
 {
     int arr[] = { 1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9 };
